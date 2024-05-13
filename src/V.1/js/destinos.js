7b4ef40729f1960a01402598acd28197cc060011
David Mangas Hernandez <148961050+davidmh-7@users.noreply.github.com>
2024-05-13 13:06:32 +0200

    Solucion de fallos+ Login+ Register

--- src/V.1/js/destinos.js
+++ src/V.1/js/destinos.js
@@ -1,13 +1,15 @@
-function updateDestino() {
+function updateHorariosDisponibles() {
     var origen = document.getElementById("origen");
     var destino = document.getElementById("destino");
-    var horaSalida = document.getElementById("horaSalida");
+    var horaSalidaContainer = document.getElementById("horaSalidaContainer");
+    var horaSalidaSelect = document.getElementById("horaSalida");
 
-    // Obtener el valor seleccionado en el origen y en el destino
+    var horariosDisponibles = [];
+
+    // Obtener el origen y el destino seleccionados
     var origenValue = origen.value;
     var destinoValue = destino.value;
 
-    // Habilitar todas las opciones en ambos campos primero
     for (var i = 0; i < origen.options.length; i++) {
         origen.options[i].disabled = false;
     }
@@ -21,14 +23,169 @@ function updateDestino() {
         origen.querySelector('option[value="' + destinoValue + '"]').disabled = true;
     }
 
-    // Si se ha seleccionado una opción en el origen
     if (origenValue !== "0") {
-        // Deshabilitar la opción seleccionada en el destino
         destino.querySelector('option[value="' + origenValue + '"]').disabled = true;
-        
-        // Limpiar el campo de destino
-        destino.value = "0";
     }
-    
+
+
+    // Lógica para determinar los horarios disponibles según el origen y el destino
+    switch (origenValue) {
+        case "BILBAO":
+            switch (destinoValue) {
+                case "DONOSTIA-SAN SEBASTIAN":
+
+                    for (var hora = 6; hora <= 22; hora++) {
+                        var horaFormateada = hora < 10 ? "0" + hora : hora;
+                        var horaCompleta = horaFormateada + ":30";
+                        horariosDisponibles.push({ text: horaCompleta, value: horaCompleta });
+                    }
+                    break;
+                case "BILBAO":
+                    alertLlamada();
+                    break;
+
+                case "VITORIA-GASTEIZ":
+
+                    alertLlamada();
+                    break;
+
+                case "AEROPUERTO BILBAO":
+                    alertLlamada();
+                    break;
+
+                // Agrega más casos para otros destinos desde Bilbao si es necesario
+                default:
+                    break;
+            }
+            break;
+        case "DONOSTIA-SAN SEBASTIAN":
+            switch (destinoValue) {
+                case "BILBAO":
+
+                    for (var hora = 6; hora <= 22; hora++) {
+                        var horaFormateada = hora < 10 ? "0" + hora : hora;
+                        var horaCompleta = horaFormateada + ":00";
+                        horariosDisponibles.push({ text: horaCompleta, value: horaCompleta });
+                    }
+                    break;
+
+                case "VITORIA-GASTEIZ":
+
+                    for (var hora = 6; hora <= 22; hora++) {
+                        var horaFormateada = hora < 10 ? "0" + hora : hora;
+                        var horaCompleta = horaFormateada + ":15";
+                        horariosDisponibles.push({ text: horaCompleta, value: horaCompleta });
+                    }
+                    break;
+
+                case "AEROPUERTO BILBAO":
+
+                    for (var hora = 5; hora <= 21; hora++) {
+                        var horaFormateada = hora < 10 ? "0" + hora : hora;
+                        var horaCompleta = horaFormateada + ":15";
+                        horariosDisponibles.push({ text: horaCompleta, value: horaCompleta });
+                    }
+                    break;
+                default:
+                    break;
+            }
+            break;
+
+        case "VITORIA-GASTEIZ":
+            switch (destinoValue) {
+                case "DONOSTIA-SAN SEBASTIAN":
+
+                    for (var hora = 5; hora <= 22; hora++) {
+                        var horaFormateada = hora < 10 ? "0" + hora : hora;
+                        var horaCompleta = horaFormateada + ":45";
+                        horariosDisponibles.push({ text: horaCompleta, value: horaCompleta });
+                    }
+                    break;
+                case "BILBAO":
+                    alertLlamada();
+                    break;
+
+                case "VITORIA-GASTEIZ":
+
+                    alertLlamada();
+                    break;
+
+                case "AEROPUERTO BILBAO":
+                    alertLlamada();
+                    break;
+                default:
+                    break;
+            }
+            break;
+
+        case "AEROPUERTO BILBAO":
+            switch (destinoValue) {
+                case "DONOSTIA-SAN SEBASTIAN":
+
+                    for (var hora = 6; hora <= 22; hora++) {
+                        var horaFormateada = hora < 10 ? "0" + hora : hora;
+                        var horaCompleta = horaFormateada + ":45";
+                        horariosDisponibles.push({ text: horaCompleta, value: horaCompleta });
+                    }
+                    break;
+                case "BILBAO":
+                    alertLlamada();
+                    break;
+
+                case "VITORIA-GASTEIZ":
+
+                    alertLlamada();
+                    break;
+
+                case "AEROPUERTO BILBAO":
+                    alertLlamada();
+                    break;
+                default:
+                    break;
+            }
+            break;
+
+
+
+        default:
+            break;
+    }
+
+    horaSalidaSelect.innerHTML = "";
+
+
+    for (var i = 0; i < horariosDisponibles.length; i++) {
+        var option = document.createElement("option");
+        option.text = horariosDisponibles[i].text;
+        option.value = horariosDisponibles[i].value;
+        horaSalidaSelect.add(option);
+    }
+
+
+    if (horariosDisponibles.length > 0) {
+        horaSalidaContainer.style.display = "grid";
+    } else {
+        horaSalidaContainer.style.display = "none";
+    }
+
+
+    function alertLlamada() {
+        var xAlert = document.getElementById("AlertQuitar");
+        xAlert.style.display = "";
+        let contador = 0;
+        let procentajeBarra = document.getElementById('progress-bar');
+        let intervalo = setInterval(() => {
+            contador++;
+
+            procentajeBarra.style.width = contador + "%";
+            if (contador >= 100) {
+                clearInterval(intervalo);
+            }
+        }, 30);
+        setTimeout(() => {
+            var xAlert = document.getElementById("AlertQuitar");
+            xAlert.style.display = "none";
+        }, 3000);
+    }
 
 }
